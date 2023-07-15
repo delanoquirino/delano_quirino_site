c6626538403a532553941b2623efd1df76cc514e
delanoquirino <delanoquirino@gmail.com>
2023-07-15 10:33:44 -0300

    feat:adding component navigation bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,6 +8,7 @@ import { FaBars, FaTimes } from 'react-icons/fa';
 interface LinkItem {
     id: number;
     name: string;
+    link: string
   }
   
   export const Navbar = () => {
@@ -16,19 +17,23 @@ interface LinkItem {
     const links = [
       {
         id: 1,
-        name: "Home",
+        name: "home",
+        link: "/"
       },
       {
         id: 2,
-        name: "About",
+        name: "about",
+        link: "about"
       },
       {
         id: 3,
-        name: "Portfolio",
+        name: "portfolio",
+        link: "portfolio"
       },
       {
         id: 5,
-        name: "Contact",
+        name: "contact",
+        link: "contact"
       },
     ];
   
@@ -36,12 +41,12 @@ interface LinkItem {
       <>
       
       <ul className="hidden md:flex justify-between">
-        {links.map(({ id, name }: LinkItem) => (
+        {links.map(({ id, name, link }: LinkItem) => (
           <li
             key={id}
             className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 hover:brightness-125 duration-200"
           >
-            <Link href={name}>
+            <Link href={link}>
               {name}
             </Link>
           </li>
@@ -52,12 +57,12 @@ interface LinkItem {
       </div>
       {nav && (
         <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-black">
-          {links.map(({ id, name }) => (
+          {links.map(({ id, name, link }) => (
             <li
               key={id}
-              className="px-4 cursor-pointer capitalize py-6 text-4xl bold hover:scale-125 hover:brightness-115 hover:font-semibold"
+              className="px-4 cursor-pointer text-white uppercase py-6 text-4xl bold hover:scale-125 hover:brightness-115 hover:font-semibold"
             >
-              <Link onClick={() => setNav(!nav)} href={name}>
+              <Link   onClick={() => setNav(!nav)} href={link}>
                 {name}
               </Link>
             </li>
