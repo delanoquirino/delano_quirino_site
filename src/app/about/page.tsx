92b0c9340b02127e63a5e8cf35a9457c7560a68d
delanoquirino <delanoquirino@gmail.com>
2023-07-16 21:15:43 -0300

    style: adjust spacing and responsiveness

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,14 +1,14 @@
 import { SkillIcon } from "@/components/SkillIcon"
 
 const About = () => {
-    return <div className="bg-black dark:bg-white">
+    return <div className="bg-black dark:bg-white md:h-screen">
         <div className="max-w-7xl m-auto p-5 flex flex-col justify-center items-center bg-black text-white dark:bg-white dark:text-black ">
             <div className="flex flex-col justify-center items-center gap-4 mt-20">
                 <h1 className="text-4xl">Sobre Mim</h1>
                 <p className="text-xl">Aqui você encontrará mais informações sobre mim e quais minhas habilidades relacionadas ao desenvolvimento web.</p>
             </div>
-            <div className="flex flex-col md:flex-row mt-20 gap-10">
-                <div className="w-full md:w-[50%]">
+            <div className="flex flex-col justify-between mx-auto md:flex-row mt-20">
+                <div className="w-full md:w-[65%]">
                     <h2 className="mb-10 text-3xl bold">Conheça-me!</h2>
                     <p className="text-base">Sou um desenvolvedor web frontend apaixonado por construir interfaces para sites e aplicativos. Meus projetos você pode conferir na page Portfolio</p>
                     <br />
@@ -16,7 +16,7 @@ const About = () => {
                     <br />
                     <p className="text-base">Se você tiver uma oportunidade que se encaixe com minhas habilidades e experiência, não hesite em entrar em contato comigo. Estou ansioso para conhecer novos desafios e colaborar em projetos empolgantes.</p>
                 </div>
-                <div><h2 className="mb-10 text-3xl bold">Minhas Habilidades</h2>
+                <div><h2 className="mb-10 text-3xl bold mt-10 md:mt-0">Minhas Habilidades</h2>
                 <div><SkillIcon/></div>
             </div>
             </div>
