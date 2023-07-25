cbad6dc5019e738b16537e42a142a857058d00f8
delanoquirino <delanoquirino@gmail.com>
2023-07-25 20:45:09 -0300

    feat: loading.tsx

---
+++ src/app/loading.tsx
@@ -0,0 +1,10 @@
+
+export default function loading () {
+  return (
+    <div className='h-screen w-full flex items-center justify-center'>
+        <p className='text-cyan-500'>
+            Loading...
+        </p>
+    </div>
+  )
+}
