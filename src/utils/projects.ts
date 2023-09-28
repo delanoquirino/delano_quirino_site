9fe9579864a84725190d630fefbbeb5b2755aa1e
delanoquirino <delanoquirino@gmail.com>
2023-09-28 15:38:49 -0300

    refactor: add project

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -1,28 +1,58 @@
 const projects = [
-    {
-      title: "Sarah Soares",
-      description: "Site criado para personal trainer Sarah Soares.",
-      imageSrc: "/projects/sarahsoares.png",
-      siteLink: "https://sarah-soares.vercel.app/",
-      githubLink: "https://github.com/delanoquirino/sarah_soares",
-      techBadges: ["Next.js", "Tailwind", "Typescript", "React Hook Form", "React Icons", "React Scroll"]
-    },
-    {
-      title: "Landing Page Leadster",
-      description: "Projeto para a vaga de Desenvolvedor Front-end da Leadster",
-      imageSrc: "/projects/leadster.png",
-      siteLink: "https://teste-leadster-two.vercel.app/",
-      githubLink: "https://github.com/delanoquirino/teste-leadster",
-      techBadges: ["Typescript", "Styled Components", "Next.js", "AntDesign", "React Icons"]
-    },
-    {
-      title: "CineMovie",
-      description: "Projeto consumindo a API TMDB para listagem dos melhores filmes",
-      imageSrc: "/projects/cinemovie.png",
-      siteLink: "https://cine-movie-rho.vercel.app/",
-      githubLink: "https://github.com/delanoquirino/CineMovie",
-      techBadges: ["Next.js", "Styled Components", "React Icons", "Fetch"]
-    },
-  ];
-  
-  export default projects;
+  {
+    title: "Amorim, Moura & martins",
+    description:
+      "Este projeto envolve o desenvolvimento de um site para o escritório de advocacia Amorim, Moura & Martins",
+    imageSrc: "/projects/ammadv1.png",
+    siteLink: "https://amorimmouramartinsadv.vercel.app/",
+    githubLink: "https://github.com/delanoquirino/amorimmouramartinsadv",
+    techBadges: [
+      "Next.js",
+      "Styled Components",
+      "Typescript",
+      "Antd",
+      "React Icons",
+      "Swiper",
+    ],
+  },
+  {
+    title: "Sarah Soares",
+    description: "Site criado para personal trainer Sarah Soares.",
+    imageSrc: "/projects/sarahsoares.png",
+    siteLink: "https://sarah-soares.vercel.app/",
+    githubLink: "https://github.com/delanoquirino/sarah_soares",
+    techBadges: [
+      "Next.js",
+      "Tailwind",
+      "Typescript",
+      "React Hook Form",
+      "React Icons",
+      "React Scroll",
+    ],
+  },
+  {
+    title: "Landing Page Leadster",
+    description: "Projeto para a vaga de Desenvolvedor Front-end da Leadster",
+    imageSrc: "/projects/leadster.png",
+    siteLink: "https://teste-leadster-two.vercel.app/",
+    githubLink: "https://github.com/delanoquirino/teste-leadster",
+    techBadges: [
+      "Typescript",
+      "Styled Components",
+      "Next.js",
+      "AntDesign",
+      "React Icons",
+    ],
+  },
+  {
+    title: "CineMovie",
+    description:
+      "Projeto consumindo a API TMDB para listagem dos melhores filmes",
+    imageSrc: "/projects/cinemovie.png",
+    siteLink: "https://cine-movie-rho.vercel.app/",
+    githubLink: "https://github.com/delanoquirino/CineMovie",
+    techBadges: ["Next.js", "Styled Components", "React Icons", "Fetch"],
+  },
+];
+
+export default projects;
